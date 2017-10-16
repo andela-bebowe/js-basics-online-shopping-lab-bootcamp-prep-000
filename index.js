9a495bd0729e12f8b9e9fb266d14a2729773a372
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-16 16:46:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.");
   } else {
+    cart = []
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   }
 }
